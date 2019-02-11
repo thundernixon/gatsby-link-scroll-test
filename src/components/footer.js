b5ddee250a30e4e6354037dde47a8710f89b082d
Stephen Nixon <swr410@gmail.com>
2019-02-10 21:45:09 -0500

    simplify footer and pagination

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,75 +1,56 @@
-import React from 'react'
-import styled from 'styled-components'
-import sizes from '../_vars/sizes'
-import colors from '../_vars/colors'
+import React from 'react';
+import styled from 'styled-components';
+import sizes from '../_vars/sizes';
+import colors from '../_vars/colors';
 
-import '../layouts/global.scss'
+import '../layouts/global.scss';
 
 const StyledFooter = styled.div`
-  background: #222;
-  color: white;
-  /* height: 200px; */
-  padding: ${sizes.padding1};
-  z-index: 2;
+	background: #222;
+	color: white;
+	/* height: 200px; */
+	padding: ${sizes.padding1};
+	z-index: 2;
 
-  .main-column {
-    display: flex;
-    justify-content: space-between;
-    flex-flow: row wrap;
-    div {
-      max-width: 300px;
-      min-width: 250px;
+	.main-column {
+		display: flex;
+		justify-content: space-between;
+		flex-flow: row wrap;
+		div {
+			max-width: 300px;
+			min-width: 250px;
 
-      &:first-of-type {
-        margin-right: 1rem;
-      }
-      &:last-of-type {
-        margin-bottom: calc(4 * ${sizes.padding1});
-      }
-    }
-  }
+			&:first-of-type {
+				margin-right: 1rem;
+			}
+			&:last-of-type {
+				margin-bottom: calc(4 * ${sizes.padding1});
+			}
+		}
+	}
 
-  p {
-    font-size: 1rem;
-  }
+	p {
+		font-size: 1rem;
+	}
 
-  a {
-    color: ${colors.accent};
-  }
-`
+	a {
+		color: ${colors.accent};
+	}
+`;
 const Footer = ({ className }) => (
-  <StyledFooter>
-    <div className="main-column">
-      <div>
-        <p>
-          <a
-            href="http://typemedia.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            TypeMedia
-          </a>{' '}
-          class of 2018
-        </p>
-        <p>All type designs the exclusive copyright of their creators.</p>
-      </div>
-      <div>
-        <p>
-          Site designed by{' '}
-          <a href="http://www.katjaschimmel.de/">Katja Schimmel</a>,{' '}
-          <a href="https://dribbble.com/zrin">Zrinka Buljubašić</a>,{' '}
-          <a href="http://genramirez.com/">Gen Ramirez</a>,{' '}
-          <a href="http://cargocollective.com/namratago">Namrata Goyal</a>, and{' '}
-          <a href="https://stephennixon.com/">Stephen Nixon</a> with
-          contributions from all of TypeMedia ’18. Site developed by Stephen
-          Nixon.
-        </p>
-        <p>
-          Typeface for text and navigation is [awesome typeface], generously
-          donated by [wonderful foundry].
-        </p>
-      </div>
-    </div>
-  </StyledFooter>
-)
-export default Footer
+	<StyledFooter>
+		<div className="main-column">
+			<div>
+				<p>Gatsby Link Scroll Test</p>
+				<p>This site is a design in progress.</p>
+			</div>
+			<div>
+				<p>
+					Site created to test GatsbyJS Issue <a href="https://github.com/gatsbyjs/gatsby/issues/7454">#7454</a>.
+				</p>
+				<p>Typeset in IBM Plex.</p>
+			</div>
+		</div>
+	</StyledFooter>
+);
+export default Footer;
